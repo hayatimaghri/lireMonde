f4da6224a3697f44b02e7227ea6e7eeae0852b3b
hayatimaghri <hayatimaghri04@gmail.com>
2026-05-20 22:26:37 +0100

    affichage de modal

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const ULR_API="http://localhost:3000/books";
 
+let allBooks = [];
 
 async function fetchBooks() {
   try {
@@ -7,6 +8,7 @@ async function fetchBooks() {
     if (!res.ok) throw new Error("Erreur serveur");
    const books = await res.json();
    renderBooks(books);
+   allBooks = books; 
   } catch (err) {
     console.log("Impossible de charger les commandes", "error");
   }
@@ -24,7 +26,7 @@ function renderBooks(books){
 
             <p>${book.auteur}</p>
 
-            <button>${book.genre}</button>
+           <button class="genre-btn" data-id="${book.id}"> ${book.genre}</button>
 
         </div>
 
@@ -32,3 +34,31 @@ function renderBooks(books){
     })
 }
 fetchBooks() ;
+// modal
+
+function openModal(book){
+  document.getElementById("modal").classList.remove("hidden");
+
+  document.getElementById("modalImg").src = book.couverture;
+  document.getElementById("modalTitle").textContent = book.titre;
+  document.getElementById("modalAuthor").textContent = book.auteur;
+  document.getElementById("modalGenre").textContent = book.genre;
+  document.getElementById("modalDesc").textContent = book.description;
+}
+
+document.getElementById("livres").addEventListener("click", async (e) => {
+  if (e.target.classList.contains("genre-btn")) {
+    
+    const id = e.target.dataset.id;
+
+    const res = await fetch(`${ULR_API}/${id}`);
+    const book = await res.json();
+
+    openModal(book);
+  }
+});
+
+document.getElementById("closeModal").onclick = () => {
+  document.getElementById("modal").classList.add("hidden");
+};
+
